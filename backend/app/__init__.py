2eedfbd51b8cc11d2f076466417cd0e3a8780dc6
Neo Hui Xuan <neo.huixuan24@gmail.com>
2025-09-22 10:42:58 +0800

    feat: connected to postgresql database

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_migrate import Migrate
+from .models import db
 from .routes.hello import hello_bp
+
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object("config.Config")
     CORS(app) 
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     # Register blueprints
     app.register_blueprint(hello_bp, url_prefix="/api")
---
+++ backend/app/models.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
