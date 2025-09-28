b91851ec6f4a5ce6673339f89787e4996f85db47
Neo Hui Xuan <neo.huixuan24@gmail.com>
2025-09-28 16:57:07 +0800

    fix: deleted useless service

--- backend/app/services/attachments_services.py
+++
@@ -1,7 +0,0 @@
-from werkzeug.utils import secure_filename
-
-def save_attachments(task, files):
-    attachments = []
-    
-    for file_storage in files:
-        filename = secure_filename(file_storage)
